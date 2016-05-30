c6987cebc9b7ace9179bde42dcfdab170e7c2394
Vikranth Srivatsa <rama22446@gmail.com>
2016-05-29 23:42:53 -0700

    Add time reduction score

--- Virus_Attack/src/ScoreBoard.java
+++ Virus_Attack/src/ScoreBoard.java
@@ -1,5 +1,4 @@
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Calendar;
 
 
@@ -18,33 +17,50 @@ public class ScoreBoard {
         initalTime = Integer.parseInt(sdf.format(cal.getTime())) * 60 + Integer.parseInt(seconds.format(cal.getTime()));
     }
 
+    boolean changed = false;
+
     public String everTurn() {
         Calendar cal = Calendar.getInstance();
         SimpleDateFormat sdf = new SimpleDateFormat("mm");
         SimpleDateFormat seconds = new SimpleDateFormat("ss");
         int newTime = Integer.parseInt(sdf.format(cal.getTime())) * 60 + Integer.parseInt(seconds.format(cal.getTime()));
         time = newTime - initalTime - minutes * 60;
-        if (time == 60) {
+        if (time == 60 && !changed) {
             minutes++;
             time = time % 60;
+            reduceScore();
+            changed = true;
+        }
+        if (time == 30 && !changed) {
+            reduceScore();
+            changed = true;
+        }
+        if (time == 31 || time == 1) {
+            changed = false;
         }
         if ((minutes + "").length() == 1) return 0 + "" + minutes + ":" + time;
         else return minutes + ":" + time;
     }
 
+    private void reduceScore() {
+        System.out.println(totalScore);
+        totalScore = totalScore - 1;
+        System.out.println(totalScore);
+    }
+
 
     public void increaseWhiteDeaths() {
         whiteCellsKilled++;
-        totalScore+=10;
+        totalScore += 10;
     }
 
     public void increaseRedCellsDead() {
         redCellsConquered++;
-        totalScore+=5;
+        totalScore += 5;
     }
 
     public String getTime() {
-        return minutes+":"+time;
+        return minutes + ":" + time;
     }
 
     public int getWhiteCellsKilled() {
